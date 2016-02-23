583635d523056881dcfdf4e1242c43f8041f79a9
MaidSafe-QA <qa@maidsafe.net>
2016-02-23 14:43:53 +0000

    Updated documentation.

--- master/implementors/std/sys/ext/thread/trait.JoinHandleExt.js
+++ master/implementors/std/sys/ext/thread/trait.JoinHandleExt.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['ctrlc'] = [];
+implementors['mio'] = [];implementors['socket_addr'] = [];implementors['crust'] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
