03575120fc2e0d6dce36379cb71e0de8377f72a5
MaidSafe-QA <qa@maidsafe.net>
2016-03-23 09:25:33 +0000

    Updated documentation.

--- master/safe_vault/non_networking_test_framework/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"mod":[["mock_routing_types",""]]});
--- master/safe_vault/types/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-initSidebarItems({"trait":[["Refreshable","This trait is required for any type (normally an account) which is refreshed on a churn event."]]});
